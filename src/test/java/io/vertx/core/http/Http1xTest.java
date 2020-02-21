9c61780718c2a3ca067cb54b1b73da93e716cce0
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2020-02-20 22:49:57 -0600

    TEST
    Signed-off-by: Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>

--- src/test/java/io/vertx/core/http/Http1xTest.java
+++ src/test/java/io/vertx/core/http/Http1xTest.java
@@ -19,11 +19,14 @@ import io.vertx.core.http.impl.HttpUtils;
 import io.vertx.core.impl.ConcurrentHashSet;
 import io.vertx.core.impl.ContextInternal;
 import io.vertx.core.impl.VertxInternal;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.*;
 import io.vertx.core.parsetools.RecordParser;
 import io.vertx.core.streams.WriteStream;
+import io.vertx.test.core.AsyncTestBase;
 import io.vertx.test.core.Repeat;
 import io.vertx.test.core.CheckingSender;
 import io.vertx.test.verticles.SimpleServer;
@@ -4771,6 +4774,7 @@ public class Http1xTest extends HttpTest {
     });
   }
 
+  private static final Logger log = LoggerFactory.getLogger(Http1xTest.class);
   @Test
   public void testHttpServerWithIdleTimeoutSendChunkedFile() throws Exception {
     // Does not pass reliably in CI (timeout)
