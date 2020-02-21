f391bafc05b77a68aa99496d82311d8bda4a396e
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2020-02-20 23:24:50 -0600

    TEST
    Signed-off-by: Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>

--- src/test/java/io/vertx/core/http/Http1xTest.java
+++ src/test/java/io/vertx/core/http/Http1xTest.java
@@ -4805,7 +4805,7 @@ public class Http1xTest extends HttpTest {
         int[] length = {0};
         resp.handler(buff -> {
           length[0] += buff.length();
-          log.info("=====>> " + length[0] + " ---> " + (now - t1));
+          log.info("=====>> " + length[0] + " ---> " + (System.currentTimeMillis() - t1));
           resp.pause();
           vertx.setTimer(1, id -> {
             resp.resume();
@@ -4813,7 +4813,7 @@ public class Http1xTest extends HttpTest {
         });
         resp.exceptionHandler(this::fail);
         resp.endHandler(v -> {
-          log.info("Termina el proceso...." + " ---> " + (now - t1));
+          log.info("Termina el proceso...." + " ---> " + (System.currentTimeMillis() - t1));
           assertEquals(expected, length[0]);
           assertTrue(System.currentTimeMillis() - now > 1000);
           testComplete();
