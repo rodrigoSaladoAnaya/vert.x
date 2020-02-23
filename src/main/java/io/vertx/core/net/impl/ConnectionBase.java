6a71daef7171d7169713029d49b7dbf940e2e048
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2020-02-23 00:13:01 -0600

    TEST
    Signed-off-by: Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>

--- src/main/java/io/vertx/core/net/impl/ConnectionBase.java
+++ src/main/java/io/vertx/core/net/impl/ConnectionBase.java
@@ -359,7 +359,7 @@ public abstract class ConnectionBase {
    * perform extra work when the idle event happens.
    */
   protected void handleIdle() {
-    log.info("xxxxx 0.1) " + chctx);
+    log.info("xxxxx 0.1) " + chctx + ", " + chctx.getClass());
     chctx.close();
   }
 
