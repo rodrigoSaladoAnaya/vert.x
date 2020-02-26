308b96c94e6e24f73c85ab7b352fcfab08605115
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2020-02-25 21:33:46 -0600

    TEST
    Signed-off-by: Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>

--- src/test/java/io/vertx/core/http/Http1xTest.java
+++ src/test/java/io/vertx/core/http/Http1xTest.java
@@ -4778,7 +4778,7 @@ public class Http1xTest extends HttpTest {
 
   @Test
   public void testHttpServerWithIdleTimeoutSendChunkedFile() throws Exception {
-    // Does not pass reliably in CI (timeout) Z: 20
+    // Does not pass reliably in CI (timeout) Z: 21
     Assume.assumeFalse(vertx.isNativeTransportEnabled());
     int expected = 16 * 1024 * 1024; // We estimate this will take more than 200ms to transfer with a 1ms pause in chunks
     File sent = TestUtils.tmpFile(".dat", expected);
